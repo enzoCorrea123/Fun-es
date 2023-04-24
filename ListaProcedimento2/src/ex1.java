f0210a13369e479fb4f06c9b89e4b0f7600b38b7
enzoCorrea123 <enzorufino1508@gmail.com>
2023-04-24 14:16:55 -0300

    adicionando projeto completo

--- ListaProcedimento2/src/ex1.java
+++ ListaProcedimento2/src/ex1.java
@@ -2,18 +2,32 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class ex1 {
-	static Random random = new Random(1001);
-	static int numRandom = random.nextInt();
-	
+	static Random random = new Random();
+	static int numRandom = random.nextInt(1001);
+	static Scanner sc = new Scanner(System.in);
+
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		System.out.println(numRandom);
 		System.out.println("Será sorteado um número de 1 a 1000, digite um palpite:");
 		int palpite = sc.nextInt();
-		while(palpite == numRandom) {
-			
-		}
-		
+		tentativas(palpite);
 
+	}
+
+	static void tentativas(int palpite) {
+
+		int contTentativas = 0;
+		while (palpite != numRandom) {
+			contTentativas++;
+			if (palpite < numRandom) {
+				System.out.println("É maior");
+			} else {
+				System.out.println("É menor");
+			}
+			System.out.println("Será sorteado um número de 1 a 1000, digite um palpite:");
+			palpite = sc.nextInt();
+		}
+		System.out.println("Você acertou!!\nNúmero de tentativas: " + contTentativas);
 	}
 
 }
